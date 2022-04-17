528e09da1d3125fb997ca2866d6ae730cd94bd11
mkozhevnikov <mkozhevnikov@servicetitan.com>
2022-04-16 17:14:48 -0700

    lesson 3

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
@@ -7,6 +8,16 @@ namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
+        /// <summary>
+        /// Maximum number of points we will allow in a series
+        /// </summary>
+        public const int MaxPoints = 250;
+
+        /// <summary>
+        /// Incrementing counter we use to plot along the X-axis
+        /// </summary>
+        private int xPosCounter = 0;
+
         #region Messages
 
         public class InitializeChart
@@ -32,6 +43,19 @@ namespace ChartApp.Actors
             }
         }
 
+        /// <summary>
+        /// Remove an existing <see cref="Series"/> from the chart
+        /// </summary>
+        public class RemoveSeries
+        {
+            public string SeriesName { get; }
+
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+        }
+
         #endregion
 
         private readonly Chart _chart;
@@ -48,6 +72,8 @@ namespace ChartApp.Actors
 
             Receive<InitializeChart>(HandleInitialize);
             Receive<AddSeries>(HandleAddSeries);
+            Receive<RemoveSeries>(HandleRemoveSeries);
+            Receive<Metric>(HandleMetrics);
         }
 
         #region Individual Message Type Handlers
@@ -56,35 +82,87 @@ namespace ChartApp.Actors
         {
             if (ic.InitialSeries != null)
             {
-                //swap the two series out
+                // swap the two series out
                 _seriesIndex = ic.InitialSeries;
             }
 
-            //delete any existing series
+            // delete any existing series
             _chart.Series.Clear();
 
-            //attempt to render the initial chart
+            // set the axes up
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
+            // attempt to render the initial chart
             if (_seriesIndex.Any())
             {
                 foreach (var series in _seriesIndex)
                 {
-                    //force both the chart and the internal index to use the same names
+                    // force both the chart and the internal index to use the same names
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
         }
 
         private void HandleAddSeries(AddSeries series)
         {
-            if (!string.IsNullOrEmpty(series.Series.Name) &&
-                !_seriesIndex.ContainsKey(series.Series.Name))
+            if(!string.IsNullOrEmpty(series.Series.Name) &&
+               !_seriesIndex.ContainsKey(series.Series.Name))
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
             }
         }
 
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) &&
+                _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && 
+                _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while(series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
         #endregion
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
     }
 }
---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,78 @@
+﻿using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    #region Reporting
+
+    /// <summary>
+    /// Signal used to indicate that it's time to sample all counters
+    /// </summary>
+    public class GatherMetrics
+    {
+    }
+
+    /// <summary>
+    /// Metric data at the time of sample
+    /// </summary>
+    public class Metric
+    {
+        public string Series { get; }
+
+        public float CounterValue { get; }
+
+        public Metric(string series, float counterValue)
+        {
+            CounterValue = counterValue;
+            Series = series;
+        }
+    }
+
+    #endregion
+
+    #region Performance Counter Management
+
+    /// <summary>
+    /// All types of counters supported by this example
+    /// </summary>
+    public enum CounterType
+    {
+        Cpu,
+        Memory,
+        Disk
+    }
+
+    /// <summary>
+    /// Enables a counter and begins publishing values to <see cref="Subscriber"/>.
+    /// </summary>
+    public class SubscribeCounter
+    {
+        public CounterType Counter { get; }
+
+        public IActorRef Subscriber { get; }
+
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+    }
+
+    /// <summary>
+    /// Unsubscribes <see cref="Subscriber"/> from receiving updates 
+    /// for a given counter
+    /// </summary>
+    public class UnsubscribeCounter
+    {
+        public CounterType Counter { get; }
+
+        public IActorRef Subscriber { get; }
+
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+    }
+
+    #endregion
+}
